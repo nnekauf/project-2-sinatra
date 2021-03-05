b81b0452fe9e1bbaeeb3d48662273ca19e27568d
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-03-05 02:26:12 -0500

    adds draft of the different methods OutfitsController should have at the moment to be filled in

---
+++ app/controllers/outfits_controller.rb
@@ -0,0 +1,21 @@
+class OutfitsController < ApplicationController
+    get 'outfits' do
+
+    end
+
+    get 'outfits/new' do
+
+    end
+
+    get 'outfits/:id' do
+
+    end
+
+    get 'outfits/:id/edit' do
+
+    end
+
+    delete 'outfits/:id' do
+
+    end
+end
