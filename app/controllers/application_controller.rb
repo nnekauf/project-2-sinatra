f9330267c1dad872328415cde113986103056daf
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-03-05 00:48:05 -0500

    adds /failure route which renders failure view page that allows user to try logging in or signup again when the first attempt has failed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,10 @@ class ApplicationController < Sinatra::Base
     erb :login
   end
 
+  get "/failure" do
+    erb :failure
+  end
+
   post 'login' do
     user = User.find_by(username: params[:username])
 		if user && user.authenticate(params[:password])
