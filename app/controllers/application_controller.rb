ee899af53ad09bdca3b396655020da80a2fdb60a
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-03-05 00:54:38 -0500

    adds helper methods #logged_in? and #current_user to controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -55,4 +55,14 @@ class ApplicationController < Sinatra::Base
     redirect "/"
   end
 
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+  
 end
