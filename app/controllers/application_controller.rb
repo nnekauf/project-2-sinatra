5d319ea18b6b282a7302d04b56a03573b3710380
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-03-05 00:31:21 -0500

    enables sessions and creates post'/login' route which authenticates user and brings them to their profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
   end
 
   get "/" do
@@ -13,6 +15,16 @@ class ApplicationController < Sinatra::Base
 
   get "/login" do
     erb :login
+  end
+
+  post 'login' do
+    user = User.find_by(username: params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/profile"
+		  else
+			redirect "/failure"
+		  end
   end
 
   get '/signup' do
