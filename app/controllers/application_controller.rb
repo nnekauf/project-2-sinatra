4055e4985471585af01eda9963c63710b407072d
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-03-05 22:42:55 -0500

    adds helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -67,4 +67,14 @@ class ApplicationController < Sinatra::Base
     end
   end
   
+  helpers do
+    def current_user
+      @user = User.find_by(id: session[:user_id])
+    end
+
+    def redirect_if_not_logged_in
+        redirect '/login' unless current_user
+    end
+
+  end
 end
