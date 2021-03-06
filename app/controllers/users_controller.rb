70d428cdb4a6ac14df41fd4ce2623b9a634a2bc3
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-03-06 17:40:52 -0500

    modifies sign up form and corresponding paths

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,20 +24,20 @@ class UsersController < ApplicationController
     end
 
     post '/signup' do
-        if params[:username] == "" || params[:password] == ""
+        if params[:user][:username] == "" || params[:user][:password] == ""
             redirect to "/failure"
          else
-            user = User.create(username: params[:username], password: params[:password])
+            user = User.create(params[:user])
             session[:user_id] = user.id
-            redirect to "/profile/#{user.id}"
+            redirect to "/users/#{user.id}"
         end
     end
 
-    get 'users/profile/:id' do
-        "hello!"
+    get 'users/:id' do
+        # "if they have outfits, see all outfit names here with their link"
         @user = User.find_by(id: params[:id])
-        # @outfits = @user.outfits
-        erb :profile
+         @outfits = @user.outfits
+         erb :'/users/show'
     end
 
     get 'users/logout' do
