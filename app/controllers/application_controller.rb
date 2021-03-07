fcf6996a64adefc49a7d1b6a8d2df7ab6dcbe03c
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-03-06 23:55:30 -0500

    adjusts navigation bar based on if user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,9 +24,6 @@ class ApplicationController < Sinatra::Base
  
 
   helpers do
-    def logged_in?
-      !!session[:user_id]
-    end
 
     def current_user
       @user = User.find_by(id: session[:user_id])
